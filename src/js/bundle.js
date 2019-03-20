b05d1050e2674aa535fb9456d9c56fffca4d0a03
Ode Manzi <manzii.ode@gmail.com>
2019-03-21 01:05:28 +0800

    add mobile navigation

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,73 +1,84 @@
 import Swiper from "swiper";
 import lightbox from "iframe-lightbox";
 var mySwiper = new Swiper(".slider", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination"
-  },
+    // If we need pagination
+    pagination: {
+        el: ".swiper-pagination"
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".slider-control-button--next",
-    prevEl: ".slider-control-button--prev"
-  },
+    // Navigation arrows
+    navigation: {
+        nextEl: ".slider-control-button--next",
+        prevEl: ".slider-control-button--prev"
+    },
 
-  effect: "coverflow",
-  autoplay: {
-    delay: 5000
-  }
+    effect: "coverflow",
+    autoplay: {
+        delay: 5000
+    }
 
-  // on: {
-  //     slideChange: function () {
-  //         /* do something */
-  //         console.log(this.slideActiveClass);
-  //     },
-  // }
+    // on: {
+    //     slideChange: function () {
+    //         /* do something */
+    //         console.log(this.slideActiveClass);
+    //     },
+    // }
 });
 
 var activitiesSwiper = new Swiper(".activities-slider", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true
-  },
-  navigation: {
-    nextEl: ".slider-control-button--next",
-    prevEl: ".slider-control-button--prev"
-  }
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+    navigation: {
+        nextEl: ".slider-control-button--next",
+        prevEl: ".slider-control-button--prev"
+    }
 });
 
 /* locations hover */
 
 let locations = document.querySelectorAll(".location"),
-  locationDataClass = ".location__data",
-  locationDataTarget = document.querySelector(".details-placeholder");
+    locationDataClass = ".location__data",
+    locationDataTarget = document.querySelector(".details-placeholder");
 // location.addEventListener('mouseover', function () {
 //     console.log('Oh okay!');
 // })
 
-locations.forEach(function(element, index) {
-  let locationData = element.querySelector(locationDataClass);
-  element.addEventListener("mouseover", function() {
-    locationDataTarget.innerHTML = locationData.innerHTML;
-    // locationDataTarget.innerHTML = '';
-  });
+locations.forEach(function (element, index) {
+    let locationData = element.querySelector(locationDataClass);
+    element.addEventListener("mouseover", function () {
+        locationDataTarget.innerHTML = locationData.innerHTML;
+        // locationDataTarget.innerHTML = '';
+    });
 
-  element.addEventListener("mouseout", function() {
-    locationDataTarget.innerHTML =
-      "<p>Hover Over Province for more information</p>";
-    // locationDataTarget.innerHTML = '';
-  });
+    element.addEventListener("mouseout", function () {
+        locationDataTarget.innerHTML =
+            "<p>Hover Over Province for more information</p>";
+        // locationDataTarget.innerHTML = '';
+    });
 });
 
 [].forEach.call(
-  document.getElementsByClassName("iframe-lightbox-link"),
-  function(el) {
-    el.lightbox = new IframeLightbox(el);
-  }
+    document.getElementsByClassName("iframe-lightbox-link"),
+    function (el) {
+        el.lightbox = new IframeLightbox(el);
+    }
 );
+
+
+/* menu */
+
+const menuIcon = document.querySelector('.menu-icon');
+const mobileNavigation = document.querySelector('.mobile-navigation');
+
+menuIcon.addEventListener('click', function (e) {
+    this.classList.toggle('mobile-menu-open');
+    mobileNavigation.classList.toggle('mobile-menu-open');
+})   
